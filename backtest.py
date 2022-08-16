1257d704d2246070b80416aafbca47c987e1ad4d
tanger-lm <eric.tang@lemon.markets>
2022-08-16 15:08:17 +0200

    cleanup files

--- backtest.py
+++ backtest.py
@@ -89,15 +89,15 @@ if __name__ == '__main__':
 
     x_train, y_train, x_test, y_test, test_data_frames = get_data('data/aapl.csv', num_hours=num_hours)
 
-    # bt = Backtest(
-    #     data=test_data_frames,
-    #     strategy=DenseNeuralNetTrader,
-    #     cash=100000,
-    #     commission=0,
-    #     exclusive_orders=True
-    # )
-    # dense_output = bt.run()
-    # bt.plot()
+    bt = Backtest(
+        data=test_data_frames,
+        strategy=DenseNeuralNetTrader,
+        cash=100000,
+        commission=0,
+        exclusive_orders=True
+    )
+    dense_output = bt.run()
+    bt.plot()  # generate an informative HTML graphic for your backtest
 
     bt = Backtest(
         data=test_data_frames,
